f212a23304cbeddd259948c3f9ce7ddbcb62fe95
Kai Konishi-Dukes <kaikdukes@gmail.com>
2022-11-28 20:32:58 +0000

    react-map-gl

--- components/MapboxMap.tsx
+++ components/MapboxMap.tsx
@@ -1,30 +1,19 @@
 import React, { MutableRefObject, useEffect, useRef, useState } from "react";
-import mapboxgl, { Map } from "mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
+import mapboxgl from "mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
+import Map from "react-map-gl";
+
 mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN as string;
-console.log("hi", process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN);
 function MapboxMap() {
-  const mapContainer: any = useRef(null);
-  const map: any = useRef(null);
-  const [lng, setLng] = useState(-70.9);
-  const [lat, setLat] = useState(42.35);
-  const [zoom, setZoom] = useState(9);
-
-  useEffect(() => {
-    if (map.current) {
-      return;
-    }
-
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: "mapbox://styles/mapbox/streets-v12",
-      center: [lng, lat],
-      zoom: zoom,
-    });
-  }, []);
   return (
-    <div ref={mapContainer} className="h-96">
-      MapboxMap
-    </div>
+    <Map
+      initialViewState={{
+        longitude: -122.4,
+        latitude: 37.8,
+        zoom: 14,
+      }}
+      style={{ width: "100%", height: "500px" }}
+      mapStyle="mapbox://styles/mapbox/streets-v12"
+    />
   );
 }
 
