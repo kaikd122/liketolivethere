ae1cec7e7e9c9e76e9b18cabdf5ff6d0d7a27c08
Kai Konishi-Dukes <kaikdukes@gmail.com>
2022-12-11 11:36:54 +0000

    replace create with raw sql

--- pages/api/createReview.ts
+++ pages/api/createReview.ts
@@ -2,6 +2,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import prisma from "../../lib/prisma";
 import { createReviewArgs } from "../../lib/actions/review";
+import cuid from "cuid";
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const body: createReviewArgs = req.body;
   const { data } = body;
@@ -10,12 +11,15 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     console.log("I AM HERE");
     const nearestTown: Array<{ id: number; name: string; geom: string }> =
       await prisma.$queryRaw`SELECT id, name, geom::text FROM towns ORDER BY geom <-> st_setsrid(st_makepoint(${data.longitude},${data.latitude}),4326) LIMIT 1`;
-    const result = await prisma.review.create({
-      data: {
-        ...data,
-        townId: nearestTown[0].id,
-      },
-    });
+
+    const result =
+      await prisma.$executeRaw`INSERT INTO "Review" (id, body, title, user_id, town_id, latitude, longitude, geom, updated_at) VALUES (${cuid()}, ${
+        data.body
+      }, ${data.title}, ${data.userId}, ${nearestTown[0].id}, ${
+        data.latitude
+      }, ${data.longitude}, st_setsrid(st_makepoint(${data.longitude},${
+        data.latitude
+      }),4326), current_timestamp);`;
     res.status(200).json(result);
   } catch (err) {
     console.log(err);
