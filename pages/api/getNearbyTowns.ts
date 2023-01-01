45657071a63cabf830f1b02a01b2ce3d470103e1
Kai Konishi-Dukes <kaikdukes@gmail.com>
2023-01-01 23:58:36 +0000

    logs in get nearby

--- pages/api/getNearbyTowns.ts
+++ pages/api/getNearbyTowns.ts
@@ -5,11 +5,14 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   const body: getNearbyTownsArgs = req.body;
   const { data } = body;
   try {
+    console.log("GET NEARBY TOWNS", data);
     const result: Array<{ id: number; name: string; geom: string }> =
       await prisma.$queryRaw`SELECT id, name, geom::text FROM towns ORDER BY geom <-> st_setsrid(st_makepoint(${data.longitude},${data.latitude}),4326) LIMIT ${data.limit}`;
+    console.log("RES", result);
     res.status(200).json(result);
   } catch (err) {
     console.log(err);
+    console.log("ERROR WHEN GETTING NEARBY");
     res.status(403).json({ err: "Error occured while fetching nearby towns" });
   }
 };
