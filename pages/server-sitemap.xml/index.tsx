030cdc37f166b3beec3bfce6f3a4ab6e89e1b536
Kai Konishi-Dukes <kaikdukes@gmail.com>
2023-01-02 00:57:14 +0000

    getalltowns

--- pages/server-sitemap.xml/index.tsx
+++ pages/server-sitemap.xml/index.tsx
@@ -4,6 +4,7 @@ import { getServerSideSitemap } from "next-sitemap";
 import { GetServerSideProps } from "next";
 import { getAllTownsRequest } from "../../lib/actions/search";
 import { getTownUrl } from "../../lib/util/urls";
+import prisma from "../../lib/prisma";
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   // Method to source urls from cms
@@ -12,16 +13,20 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
   let fields: any = [];
 
   try {
-    const res = await getAllTownsRequest({ blank: true });
-    console.log("RES", res);
-    if (res.ok) {
-      const towns: { id: number; name: string }[] = await res.json();
-      fields = towns.map((town) => ({
-        loc: `https://liketolivethere.com${getTownUrl(town)}`,
-        lastmod: new Date().toISOString(),
-        priority: 0.8,
-      }));
-    }
+    const towns = await prisma.towns.findMany({
+      select: {
+        id: true,
+        name: true,
+      },
+    });
+
+    console.log("RES", towns.length);
+
+    fields = towns.map((town) => ({
+      loc: `https://liketolivethere.com${getTownUrl(town)}`,
+      lastmod: new Date().toISOString(),
+      priority: 0.8,
+    }));
   } catch (error) {
     console.log(error);
   }
